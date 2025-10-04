efc07871cc5e7c2f92bcd263257132c411794ec1
M-Harish05 <harish.musidipilli@gmail.com>
2025-10-04 18:23:12 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
@@ -20,3 +20,4 @@ export default defineConfig({
   }
 
 });
+
